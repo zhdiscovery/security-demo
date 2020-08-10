9acc9368e0c56237f8634116bea1cf1d825e8654
zh <277708272@qq.com>
2020-08-10 11:31:55 +0800

    update login

--- src/main/java/com/zh/controller/UserController.java
+++ src/main/java/com/zh/controller/UserController.java
@@ -2,17 +2,10 @@ package com.zh.controller;
 
 import com.zh.common.CommonResult;
 import com.zh.common.ResultCode;
+import com.zh.dto.UmUserLoginParam;
 import com.zh.entity.UmUser;
 import com.zh.service.UmUserService;
-import com.zh.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,27 +20,15 @@ public class UserController {
     @Autowired
     private UmUserService umUserService;
 
-    @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
-    @Autowired
-    private JwtUtil jwtUtil;
-
     @PostMapping("/login")
-    public CommonResult<Map<String, Object>> login(String username, String password) {
-        if (StringUtils.isEmpty(username) || StringUtils.isEmpty(password)) {
-            return CommonResult.failed(ResultCode.VALIDATE_FAILED.getCode(), "username or password error");
+    public CommonResult<Map<String, Object>> login(@RequestBody UmUserLoginParam umUser) {
+        if (StringUtils.isEmpty(umUser.getUsername()) || StringUtils.isEmpty(umUser.getPassword())) {
+            return CommonResult.failed(ResultCode.VALIDATE_FAILED.getCode(), "empty username or password");
         }
-        UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
-            return CommonResult.failed(ResultCode.VALIDATE_FAILED.getCode(), "password error");
+        String token = umUserService.login(umUser.getUsername(), umUser.getPassword());
+        if (token == null) {
+            return CommonResult.validateFailed("invalid username or password");
         }
-        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        String token = jwtUtil.generateToken(userDetails);
         Map<String, Object> result = new HashMap<>();
         result.put("token", token);
         return CommonResult.success(result);
--- src/main/java/com/zh/service/impl/UmUserServiceImpl.java
+++ src/main/java/com/zh/service/impl/UmUserServiceImpl.java
@@ -4,12 +4,20 @@ import com.zh.dao.UmUserDAO;
 import com.zh.entity.UmPermission;
 import com.zh.entity.UmUser;
 import com.zh.service.UmUserService;
+import com.zh.utils.JwtUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 
@@ -17,11 +25,17 @@ import java.util.List;
 public class UmUserServiceImpl implements UmUserService {
     private static final Logger LOGGER = LoggerFactory.getLogger(UmUserServiceImpl.class);
 
-    @Autowired
+    @Resource
     private UmUserDAO umUserDAO;
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
 
     @Override
     public UmUser getUserByUsername(String username) {
@@ -44,7 +58,19 @@ public class UmUserServiceImpl implements UmUserService {
 
     @Override
     public String login(String username, String password) {
-        return null;
+        String token = null;
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+                throw new BadCredentialsException("wrong password");
+            }
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            token = jwtUtil.generateToken(userDetails);
+        } catch (UsernameNotFoundException e) {
+            LOGGER.warn("登录异常：{}", e.getMessage());
+        }
+        return token;
     }
 
     @Override
